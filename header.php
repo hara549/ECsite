8ea9a17bbbd840fe2d3c19c15182fe88b526ece3
yhara <y.harata3@gmail.com>
2019-07-08 07:33:30 +0900

    ハンバーガーメニューを非表示に変更

--- header.php
+++ header.php
@@ -2,7 +2,13 @@
   echo '
     <header>
       <div class="header_left"><a href="index.php">Shopping Site</a></div>
-      <span class="fa fa-bars menu-icon"></span>
+      <!--
+      <button type="button" class="drawer-toggle drawer-hamburger">
+        <span class="sr-only">toggle navigation</span>
+        <span class="drawer-hamburger-icon"></span>
+      </button>
+      -->
+
       <div id="menu">
         <ul>
           <li><a href="index.php"><i class="fas fa-search"></i></br>商品一覧</a></li>
@@ -10,6 +16,13 @@
           <li><a href="http://localhost/phpmyadmin/" target="_blonk"><i class="fas fa-cog"></i></br>管理画面</a></li>
         </ul>
       </div>
+      <nav class="drawer-nav">
+        <ul class="drawer-menu">
+          <li><a href="index.php">商品一覧</a></li>
+          <li><a href="cart.php">カート</a></li>
+          <li><a href="http://localhost/phpmyadmin/ ">管理画面</a></li>
+        </ul>
+      </nav>
     </header>
   '
 ?>
